e023d2dbe40edc98c6908e8558cacca3be9a95dd
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2020-01-07 09:35:32 -0500

    chore(bdd): demo curl

--- app/src/components/user/service/section.service.ts
+++ app/src/components/user/service/section.service.ts
@@ -3,29 +3,10 @@ import { User } from '../entity/user.entity';
 import { Section } from '../entity/section.entity';
 
 export const getSectionByUser = async (idUser) => {
-
     return await getRepository(User).findOne(idUser, { relations: ["sections"] })
-
-    // let roleRepository = getRepository(Role);
-    // let role = await roleRepository.findOne({ id: params.idRole })
-
-    // let user = new User();
-    // user.name = params.name
-    // user.surname = params.surname;
-    // user.email = params.email;
-    // user.username = params.username;
-    // user.password = await generateHash(params.password, 10);
-    // user.photo = params.photo;
-    // user.role = role;
-
-    // let userRepository = getRepository(User);
-
-    // return await userRepository.save(user);
 }
 
 export const addSectionByUser = async (idUser, sectionsIds) => {
-
-    console.log('sectionsIds', sectionsIds)
 
     let sections = sectionsIds.map(idSection => {
         let section = new Section()
@@ -34,40 +15,8 @@ export const addSectionByUser = async (idUser, sectionsIds) => {
     })
 
     let user = new User();
-    // user.id = idUser;
     user.sections = sections;
 
-
     let userRepository = getRepository(User);
-    // let role = await userRepository.
-
-
-    return await userRepository.save(user)
-
-
-
+    return await userRepository.save(user);
 }
-
-// export class Section {
-//     @PrimaryGeneratedColumn()
-//     id: number;
-
-//     @Column()
-//     name: string;
-
-//     @Column()
-//     url: string;
-
-//     @Column()
-//     status: boolean;
-
-//     @ManyToOne(type => Module, module => module.sections)
-//     module: Module;
-
-//     @ManyToMany(type => User, user => user.sections)
-//     @JoinTable()
-//     users: User[];
-// }
-
-
-// }
